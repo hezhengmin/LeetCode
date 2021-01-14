a9aec711433e2b1eb26056cb64fb26cc9a2ba6d1
hezhengmin <zhengmin099@gmail.com>
2021-01-14 22:37:23 +0800

    String

--- JavaScript/58. Length of Last Word.js
+++ JavaScript/58. Length of Last Word.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    // trime() 字串前後兩端刪除空白字元
+    // trim() 字串前後兩端刪除空白字元
     const array = s.trim().split(" ");
     return array[array.length - 1].length;
 };
