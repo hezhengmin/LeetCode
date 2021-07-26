6c7c3955a9cf88d978972f05a05b0ac8bae34433
hezhengmin <zhengmin099@gmail.com>
2021-07-26 23:03:32 +0800

    left join

--- SQL/601. Human Traffic of Stadium.sql
+++ SQL/601. Human Traffic of Stadium.sql
@@ -1,9 +1,9 @@
-select *
+select S1.id,S1.visit_date,S1.people
 from Stadium S1
 left join Stadium S2 on S1.id = S2.id -1
 left join Stadium S3 on S1.id = S3.id -2
 left join Stadium S4 on S1.id = S4.id +1
 left join Stadium S5 on S1.id = S5.id +2
-where 
-
-
+where ((S1.people>=100) and (S2.people>=100) and (S3.people>=100)) or
+((S4.people>=100) and (S1.people>=100) and (S2.people>=100)) or
+((S5.people>=100) and (S4.people>=100) and (S1.people>=100))
